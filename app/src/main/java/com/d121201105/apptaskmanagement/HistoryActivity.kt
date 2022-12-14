146ab13710ade833bde9eb371ce5646ecc21fb54
dewayp <dewayoganteng1@gmail.com>
2022-12-14 20:43:36 +0800

    upload app task management

---
+++ app/src/main/java/com/d121201105/apptaskmanagement/HistoryActivity.kt
@@ -0,0 +1,12 @@
+package com.d121201105.apptaskmanagement
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HistoryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history)
+    }
+}
